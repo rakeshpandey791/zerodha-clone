60e5b1686be24d36f3f326a901a3eee36ab7ba18
rakeshpandey791 <rakeshpandey791@gmail.com>
2023-05-05 03:13:13 +0530

    Make custom table component more configurable and use it on order tab screen

--- src/components/molecules/stockWiseInvestment.tsx
+++ src/components/molecules/stockWiseInvestment.tsx
@@ -5,6 +5,28 @@ import { getTotalInvestment } from "../../store/marketWatchSlice";
 
 const StockWiseInvestment = () => {
   const totalInvestment = useSelector(getTotalInvestment);
+  const getClass = (index: number) => {
+    switch (index) {
+      case 0:
+        return "bg-blue-900";
+      case 1:
+        return "bg-blue-800";
+      case 2:
+        return "bg-blue-600";
+      case 3:
+        return "bg-blue-500";
+      case 4:
+        return "bg-blue-400";
+      case 5:
+        return "bg-blue-300";
+      case 6:
+        return "bg-blue-200";
+      case 7:
+        return "bg-blue-100";
+      case 8:
+        return "bg-blue-50";
+    }
+  };
   return (
     <>
       {holdingData?.data
@@ -16,9 +38,7 @@ const StockWiseInvestment = () => {
             <div
               title={stock.tradingsymbol}
               data-tooltip-target={`tooltip-default-${stock.tradingsymbol}`}
-              className={`text-sm h-14 p-4 text-yellow-500 bg-blue-${
-                900 - index * 200
-              }`}
+              className={`text-sm h-14 p-4 text-white ${getClass(index)}`}
               style={{
                 width: `${
                   (stock.average_price * stock.quantity * 100) / totalInvestment
